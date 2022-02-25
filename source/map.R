d80f24f28441ad5d73286143ae67fcb4673ba9db
ShengyueGao <shengg6@uw.edu>
2022-02-25 02:35:56 -0800

    changes

--- source/map.R
+++ source/map.R
@@ -10,14 +10,34 @@ blank_theme <- theme_bw() +
     panel.border = element_blank()      # remove border around plot
   )
 
-# Load a shapefile of U.S. states using ggplot's `map_data()` function
-state_shape <- map_data("state")
+library(ggplot2)
+library(maps)
+library(dplyr)
+library(plotly)
 
-# Create a blank map of U.S. states
-ggplot(state_shape) +
+original_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+options(timeout = 1900)
+selected_data6 <- select(original_data, c(2,4,5,51))%>%
+  filter(year == 2010)%>%
+  group_by(state)%>%
+  summarise(sum = sum(total_jail_adm_dcrp, na.rm = TRUE))
+
+map <- ggplot(state_shape) +
   geom_polygon(
-    mapping = aes(x = long, y = lat, group = group),
+    mapping = aes(x = long, y = lat, group = group, fill = selected_data6$sum),
     color = "white", # show state outlines
     size = .1        # thinly stroked
   ) +
-  coord_map() # use a map-based coordinate system
+  coord_map() 
+
+map <-map + ggtitle("A Map Showing Jail Admission in Each State of U.S. in 2010")
+map <- ggplotly(map)
+map
+
+
+
+
+
+
+
+
